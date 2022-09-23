dce63e899306e8bad24105189c9f21a4909967c4
unknown <nik22517@gmail.com>
2022-09-23 17:47:22 +0530

    responsive

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,67 +1,94 @@
 import React from "react";
-//Animation 
-import {motion} from "framer-motion";
-import {pageAnimation, titleAnim} from "../animation";
+//Animation
+import { motion } from "framer-motion";
+import { pageAnimation, titleAnim } from "../animation";
 import styled from "styled-components";
 
 const ContactUs = () => {
-    return (
-        <ContactStyle variants={pageAnimation} initial="hidden" animate="show" exit= "exit" style={{background: "#fff"}}>
-            <Title>
-                <Hide>
-                    <motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
-                </Hide>
-            </Title>
-            <div>
-            <Hide>
-               <Social variants={titleAnim}>
-               <Circle />
-                <h2>Send Us A Message</h2>
-               </Social>
-            </Hide>
-            <Hide>
-               <Social variants={titleAnim}>
-               <Circle />
-                <h2>Send A Email  </h2>
-               </Social>
-            </Hide>
-            <Hide>
-               <Social variants={titleAnim}>
-               <Circle />
-                <h2>Social Media</h2>
-               </Social>
-            </Hide>
-            </div>
-        </ContactStyle>
-    );
+  return (
+    <ContactStyle
+      variants={pageAnimation}
+      initial='hidden'
+      animate='show'
+      exit='exit'
+      style={{ background: "#fff" }}
+    >
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
+        </Hide>
+      </Title>
+      <div>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Send Us A Message</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Send A Email </h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Social Media</h2>
+          </Social>
+        </Hide>
+      </div>
+    </ContactStyle>
+  );
 };
 
-const ContactStyle = styled(motion.div) `
-    padding: 5rem 10rem;
-    color: #353535;
-    min-height: 90vh;
+const ContactStyle = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+
+  @media (max-width: 800px) {
+    padding: 1rem;
+  }
 `;
 
-const Title = styled.div `
-   margin-bottom: 4rem;
-   color: black;
-`; 
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
 
-const Hide = styled.div `
-   overflow: hidden;
+  @media (max-width: 800px) {
+    margin-bottom: 2rem;
+    text-align: center;
+  }
 `;
-const Circle = styled.div `
-   border-radius: 50%;
-   width: 3rem;
-   height: 3rem;
-   background: #353535;
+
+const Hide = styled.div`
+  overflow: hidden;
 `;
-const Social = styled(motion.div) `
-    display: flex;
-    align-items: center;
-    h2{
-        margin: 2rem;
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: #353535;
+
+  @media (max-width: 800px) {
+    width: 0rem;
+    height: 0rem;
+  }
+`;
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
+
+  @media (max-width: 800px) {
+    h2 {
+      margin: 0rem;
+      font-size: 3rem;
     }
-`
+  }
+`;
 
 export default ContactUs;
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -5,47 +5,64 @@ import { Link } from "react-router-dom";
 import athlete from "../img/athlete-small.png";
 import theracer from "../img/theracer-small.png";
 import goodtimes from "../img/goodtimes-small.png";
-//Animation 
-import {motion} from "framer-motion";
-import {pageAnimation, fade, photoAnim, lineAnim, slider, sliderContainer} from "../animation";
-import {useScroll} from "../components/useScroll";
+//Animation
+import { motion } from "framer-motion";
+import {
+  pageAnimation,
+  fade,
+  photoAnim,
+  lineAnim,
+  slider,
+  sliderContainer,
+} from "../animation";
+import { useScroll } from "../components/useScroll";
 import ScrollTop from "../components/ScrollTop";
-
 
 const OurWork = () => {
   const [element, controls] = useScroll();
   const [element2, controls2] = useScroll();
 
   return (
-    <Work variants={pageAnimation} initial="hidden" animate="show" exit="exit" style={{background: "#fff"}}>
-    <motion.div variants={sliderContainer}> 
-    <Frame1 variants={slider}></Frame1>
-    <Frame2 variants={slider}></Frame2>
-    <Frame3 variants={slider}></Frame3>
-    <Frame4 variants={slider}></Frame4>
-    </motion.div>
+    <Work
+      variants={pageAnimation}
+      initial='hidden'
+      animate='show'
+      exit='exit'
+      style={{ background: "#fff" }}
+    >
+      <motion.div variants={sliderContainer}>
+        <Frame1 variants={slider}></Frame1>
+        <Frame2 variants={slider}></Frame2>
+        <Frame3 variants={slider}></Frame3>
+        <Frame4 variants={slider}></Frame4>
+      </motion.div>
 
       <Movie>
         <motion.h2 variants={fade}>The Athlete</motion.h2>
-        <motion.div variants={lineAnim} className="line"></motion.div>
-        <Link to="/work/the-athlete">
-        <Hide>
-          <motion.img variants={photoAnim} src={athlete} alt="athlete" />
-        </Hide>
+        <motion.div variants={lineAnim} className='line'></motion.div>
+        <Link to='/work/the-athlete'>
+          <Hide>
+            <motion.img variants={photoAnim} src={athlete} alt='athlete' />
+          </Hide>
         </Link>
       </Movie>
-      <Movie ref={element} variants = {fade} animate = {controls} initial = "hidden">
+      <Movie ref={element} variants={fade} animate={controls} initial='hidden'>
         <h2>The Racer</h2>
-        <motion.div variants={lineAnim} className="line"></motion.div>
-        <Link to="/work/the-racer">
-          <img src={theracer} alt="theracer" />
+        <motion.div variants={lineAnim} className='line'></motion.div>
+        <Link to='/work/the-racer'>
+          <img src={theracer} alt='theracer' />
         </Link>
       </Movie>
-      <Movie ref={element2} variants = {fade} animate = {controls2} initial = "hidden">
+      <Movie
+        ref={element2}
+        variants={fade}
+        animate={controls2}
+        initial='hidden'
+      >
         <h2>The Good-Time</h2>
-        <motion.div variants={lineAnim} className="line"></motion.div>
-        <Link to="/work/good-times">
-          <img src={goodtimes} alt="goodtime" />
+        <motion.div variants={lineAnim} className='line'></motion.div>
+        <Link to='/work/good-times'>
+          <img src={goodtimes} alt='goodtime' />
         </Link>
       </Movie>
       <ScrollTop />
@@ -59,6 +76,13 @@ const Work = styled(motion.div)`
   padding: 5rem 10rem;
   h2 {
     padding: 1rem 0rem;
+  }
+
+  @media (max-width: 800px) {
+    padding: 1rem;
+    h2 {
+      font-size: 3rem;
+    }
   }
 `;
 
@@ -74,12 +98,21 @@ const Movie = styled(motion.div)`
     height: 70vh;
     object-fit: cover;
   }
+
+  @media (max-width: 800px) {
+    padding-bottom: 5rem;
+    padding-top: 1rem;
+    img {
+      height: 100%;
+      object-fit: cover;
+    }
+  }
 `;
-const Hide = styled.div `
+const Hide = styled.div`
   overflow: hidden;
 `;
-//Frame Animation 
-const Frame1 = styled(motion.div) `
+//Frame Animation
+const Frame1 = styled(motion.div)`
   position: fixed;
   left: 0;
   top: 10%;
@@ -89,17 +122,16 @@ const Frame1 = styled(motion.div) `
   z-index: 2;
 `;
 
-const Frame2 = styled(Frame1) `
+const Frame2 = styled(Frame1)`
   background: #ff8efb;
 `;
 
-const Frame3 = styled(Frame1) `
+const Frame3 = styled(Frame1)`
   background: #8ed2ff;
 `;
 
-const Frame4 = styled(Frame1) `
+const Frame4 = styled(Frame1)`
   background: #23d997;
 `;
-
 
 export default OurWork;
